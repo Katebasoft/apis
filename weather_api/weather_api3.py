d9a2b2c17ae3221d9f5ba8229791ed095285be46
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-22 16:36:11 +0300

    Adds a file that uses urllib3

---
+++ weather_api/weather_api3.py
@@ -0,0 +1,22 @@
+import urllib3, json
+
+city_names = ['Nairobi', 'London', 'Lagos']
+app_key = '2bc3e79bb974a007818864813f53fd35'
+
+
+
+
+def get_weather():
+	collected_data = []
+	for city in city_names:
+		http = urllib3.PoolManager()
+		url = "http://api.openweathermap.org/data/2.5/weather?q={0}&APPID={1}&units=metric".format(city, app_key)
+		r = http.request('GET', url)
+		json_data = json.loads(r.data)
+		collected_data.append([city, json_data["main"]["temp"], json_data["weather"][0]["description"]])
+	return collected_data
+
+print "City", "\t\t", "Temperature", "\t\t", "Description", "\n"
+
+for data in get_weather():
+	print  data[0], "\t\t", data[1], "\t\t", data[2], "\n"
